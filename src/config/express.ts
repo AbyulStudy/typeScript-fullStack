bc68a4bf72fa9ce15c076c8d535eaa7ab81c2528
AbyulStudy <kinsng1599@gmail.com>
2022-05-15 00:49:59 +0900

    :books:config: express configuration init

---
+++ src/config/express.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { config } from "dotenv";
+
+config();
+const app = express();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: false }));
+
+export default app;

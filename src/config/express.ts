0cd9b2b275b10eb51f2f0780792b3cf1d5889547
AbyulStudy <kinsng1599@gmail.com>
2022-05-15 00:58:31 +0900

    :umbrella:test: test code

--- src/config/express.ts
+++ src/config/express.ts
@@ -7,4 +7,8 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: false }));
 
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
 export default app;
